6f906c8c3ef927666791d6001ae84a9fa1b16abb
JL-OLEMAR <joseluis19963@gmail.com>
2022-05-18 12:37:51 -0500

    feat: Move styles

--- src/shared/GlobalStyle.js
+++ src/shared/GlobalStyle.js
@@ -1,15 +1,35 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  * {
+  @font-face {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Raleway'), url('/fonts/Raleway-Regular.woff2') format('woff2'),
+      url('/fonts/Raleway-Regular.woff') format('woff');
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    src: local('Raleway'), url('/fonts/Raleway-Bold.woff2') format('woff2'),
+      url('/fonts/Raleway-Bold.woff') format('woff');
+    font-display: swap;
+  }
+
+  *,
+  *::before,
+  *::after {
     box-sizing: border-box;
   }
 
   body {
     margin: 0;
     font-family: ${({ theme }) => theme.font.primary};
-    color: ${({ theme }) => theme.colors.grey};
     background: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.grey};
   }
 
   a {
