5b3fceb43e981818691700f7839e9f1b7aa5b1bd
JL-OLEMAR <joseluis19963@gmail.com>
2022-05-20 02:48:01 -0500

    feat: Add methods in PostsProvider

--- src/Context/PostsContext.jsx
+++ src/Context/PostsContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { createContext } from 'react'
 
 import { getPosts, getUsers } from '../services'
 
@@ -7,19 +9,58 @@ export const PostsContext = createContext({})
 
 // Provider Component
 export const PostsProvider = ({ children }) => {
-  const [posts, setPosts] = useState([])
   const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    getPosts().then(setPosts)
-  }, [setPosts])
+  const [posts, setPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     getUsers().then(setUsers)
-  }, [setUsers])
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    getPosts().then((post) => {
+      setPosts(post)
+      setIsLoading(false)
+    })
+  }, [])
+
+  // Get a user where user.id === userId return { user }
+  const getUserById = (userId) => {
+    return users.find(({ id }) => id === Number(userId))
+  }
+
+  // Get a post where post.id === postId return { post }
+  const getPostById = (postId) => {
+    return posts.find(({ id }) => id === Number(postId))
+  }
+
+  // Get posts if (post.userId === id) return [postsFiltered] else [posts]
+  const getPostsFilterByUserId = (id) => {
+    const postsFiltered = posts.filter(({ userId }) => userId === Number(id))
+
+    return postsFiltered.length > 0 ? postsFiltered : posts
+  }
+
+  // Get all posts where post.id !== userId return posts[...]
+  const getPostsFilterByPostId = (postId) => {
+    return posts.filter(({ id }) => id !== Number(postId))
+  }
 
   return (
-    <PostsContext.Provider value={{ posts, setPosts, users }}>
+    <PostsContext.Provider
+      value={{
+        users,
+        posts,
+        isLoading,
+        setPosts,
+        getUserById,
+        getPostById,
+        getPostsFilterByUserId,
+        getPostsFilterByPostId
+      }}
+    >
       {children}
     </PostsContext.Provider>
   )
