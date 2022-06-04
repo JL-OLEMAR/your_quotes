a8531e8355c47d0990670df8494cabdba8cb63f7
JL-OLEMAR <joseluis19963@gmail.com>
2022-06-04 08:26:20 -0500

    fix: useUsers

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,18 +1,15 @@
-import { useEffect, useContext, useState } from 'react'
+import { useEffect, useContext } from 'react'
 
 import { getUsers } from '../services'
 import { PostsContext } from '../Context'
 
 export function useUsers() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false)
   const { users, setUsers } = useContext(PostsContext)
 
   useEffect(() => {
     if (users.length === 0) {
-      setIsLoadingUsers(true)
       getUsers().then((user) => {
         setUsers(user)
-        setIsLoadingUsers(false)
         window.localStorage.setItem('users', JSON.stringify(user))
       })
     } else {
@@ -20,5 +17,5 @@ export function useUsers() {
     }
   }, [users, setUsers])
 
-  return { users, isLoadingUsers }
+  return { users }
 }
